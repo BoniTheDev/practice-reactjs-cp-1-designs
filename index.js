f2410b9624bbfeb499c38a2b68d0b5c6a065f650
BoniTheDev <boniramesh209@gmail.com>
2022-10-30 06:46:07 +0000

    designs-practice

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  //   Write your code here.
+  <div className="background-container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="images-card">
+      <img
+        className="image-width"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        alt="image"
+      />
+      <img
+        className="image-width"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        alt="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
